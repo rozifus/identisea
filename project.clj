5f97bcf3f98228fcf300e13c9823658ddfa31432
Ryan Miller <rozifus@gmail.com>
2015-12-14 00:11:41 +1100

    prettified, cleaned up, cartesian -> combinations

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.1"]]
   :main ^:skip-aot identisea.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/identisea/core.clj
+++ src/identisea/core.clj
@@ -1,56 +1,73 @@
 (ns identisea.core
   (:gen-class)
-  (:use [clojure.math.numeric-tower]))
+  (:use [clojure.math.numeric-tower]
+        [clojure.math.combinatorics]))
 
 (defn result-mesh
   [functions value-sets]
   (for [f functions]
-    (map (partial apply f) value-sets)))
+    (with-meta 
+      (map (partial apply f) value-sets) 
+      (meta f))))
 
-(defn add 
-  [x y]
-  (+ x y))
+(def add
+  (with-meta
+    (fn [x y] (+ x y))
+    {:func "add"}))
 
-(defn mul
-  [x y]
-  (* x y))
+(def mul
+  (with-meta
+    (fn [x y] (* x y))
+    {:func "mult"}))
 
-(defn exp
-  [x y]
-  (expt x y))
+(def exp
+  (with-meta 
+    (fn [x y] (expt x y))
+    {:func "exp"}))
 
-(defn modulo
-  [x y]
-  (mod x y))
+(def modulo
+  (with-meta
+    (fn [x y] (mod x y))
+    {:func "mod"}))
 
-(defn fermat [x y]
-  (modulo (exp x y) y))
+(def fermat
+  (with-meta 
+    (fn [x y] (modulo (exp x y) y))
+    {:func "fermat"}))
 
 (def functions [add mul exp modulo fermat])
 
-(def search-size 400)
+(def search-size 500)
 
 (def value-sets
-  (for [x (range 1 search-size)
-        y (range 1 search-size)]
+  (for [x (range 2 search-size)
+        y (range 2 search-size)]
     [x y]))
 
 (defn decimal-true
   [values]
-  (float (/ (count (filter identity values))
-            (count values)))) 
+  (let [cutoff (/ 100.0 search-size)
+        valid (count (filter identity values))
+        result (* 100 (float (/ valid (count values))))]
+    (if (> result cutoff)
+      result
+      0.0)))
 
 (defn search-mesh
-  [mesh function]
-    (for [f1 mesh 
-          f2 mesh
-          :when (not= f1 f2)]
-      (decimal-true (map function f1 f2))))
+  [mesh comparison]
+    (for [[f1 f2] (combinations mesh 2)]
+      (with-meta [(decimal-true (map comparison f1 f2))]
+                 {:f1 (:func (meta f1)) :f2 (:func (meta f2)) :comp "="})))
+
+(defn format-function-pair-result
+  [meta-val]
+    (str meta-val " " (:f1 (meta meta-val)) "-" (:f2 (meta meta-val))))
 
 (defn main []
   (let [rm (result-mesh functions value-sets)]
-    (doall (map println rm))
-    (doall (map println (search-mesh rm =)))))
+    (doall (map println 
+                (map format-function-pair-result
+                     (search-mesh rm =))))))
 
 (defn -main
   [& args]
